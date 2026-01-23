06614bdf256d6f83d7695f5e69dd4e4b58e3bce8
Lillian White <servalservalserval@proton.me>
2026-01-22 19:18:57 -0500

    post-nekoweb import fixes

--- scripts/domain-chunk.js
+++ scripts/domain-chunk.js
@@ -2,12 +2,10 @@
 
 const current = document.location.host;
 const target_nw = 'https://leviathan.nekoweb.org';
-const target_nc = 'https://levirenn.neocities.org';
 
 document.getElementById("domain").innerHTML = current;
 
 if (location.hostname == "leviathan.nekoweb.org") {
-  document.getElementById("switch-link").setAttribute('href', target_nc);
   document.getElementById("follow").setAttribute('style', 'display: inline !important');
   // https://neocities.org/site/levirenn?event_id=5211517
 } else {
